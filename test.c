b3ed0f704963217fab5efec593cb139e170c1a2f
tadamo3 <truizw@gmail.com>
2022-09-06 01:50:14 -0400

    Changement commentaire pull request

--- test.c
+++ test.c
@@ -1,2 +1,2 @@
-/* Ceci est un test pour le mode d'emploi Git */
+/* Ceci est un test pour le mode d'emploi GitHub - Ce commentaire a été modifié suite au commentaire sur le pull request */
 /* Ceci est un deuxième test pour le push */
