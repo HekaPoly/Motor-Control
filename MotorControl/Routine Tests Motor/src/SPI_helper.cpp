f2163c7a71b5cceabd8e56e8ffde9a3fa15ae834
evatremblay <eva.tremblay@polymtl.ca>
2022-11-23 14:17:43 -0500

    Finalisation SPI-helper

--- MotorControl/Routine Tests Motor/src/SPI_helper.cpp
+++ MotorControl/Routine Tests Motor/src/SPI_helper.cpp
@@ -8,35 +8,35 @@
  * @brief set default values in driver registers
  * 
  */
-void writeRegisters() 
+void write_registers() 
 {
-	writeRegister(0x00, 0x01, 0x11);
-	writeRegister(0x01, 0x00, 0x00);
-	writeRegister(0x02, 0x04, 0xFF);
-	writeRegister(0x03, 0x68, 0x00);
-	writeRegister(0x04, 0x00, 0xD7);
-	writeRegister(0x05, 0x00, 0x00);
-	writeRegister(0x06, 0x00, 0x00);
-	writeRegister(0x07, 0x00, 0x00);
-	writeRegister(0x08, 0x00, 0x00);
-	writeRegister(0x09, 0x00, 0x00);
-	writeRegister(0x0A, 0xF0, 0x00);
-	writeRegister(0x0B, 0x00, 0x00);
-	writeRegister(0x2A, 0x00, 0x00);
+	write_register(0x00, 0x01, 0x11);
+	write_register(0x01, 0x00, 0x00);
+	write_register(0x02, 0x04, 0xFF);
+	write_register(0x03, 0x68, 0x00);
+	write_register(0x04, 0x00, 0xD7);
+	write_register(0x05, 0x00, 0x00);
+	write_register(0x06, 0x00, 0x00);
+	write_register(0x07, 0x00, 0x00);
+	write_register(0x08, 0x00, 0x00);
+	write_register(0x09, 0x00, 0x00);
+	write_register(0x0A, 0xF0, 0x00);
+	write_register(0x0B, 0x00, 0x00);
+	write_register(0x2A, 0x00, 0x00);
 }
 
 /**
  * @brief To read the registers
  * 
  */
-void readRegisters() 
-{
+void read_registers() {
+	//To Read the registers
 	Serial.println("Lecture des registres");
-	Serial.println(readRegister(0x00), HEX);
-	Serial.println(readRegister(0x02), HEX);
-	Serial.println(readRegister(0x03), HEX);
-	Serial.println(readRegister(0x04), HEX);
-	Serial.println(readRegister(0x0A), HEX);
+	Serial.println(read_register(0x00), HEX);
+	Serial.println(read_register(0x02), HEX);
+	Serial.println(read_register(0x03), HEX);
+	Serial.println(read_register(0x04), HEX);
+	Serial.println(read_register(0x0A), HEX);
 	Serial.println();
 	Serial.println();
 }
@@ -49,8 +49,7 @@ void spi_setup()
 	/* SPI Setup */
 	pinMode(CS, OUTPUT);
 	SPI.begin();
-	writeRegisters();
-	/* writeDefaultRegisters(); */
+	write_registers();
 }
 
 /**
@@ -59,7 +58,7 @@ void spi_setup()
  * @param reg register
  * @return unsigned int 
  */
-unsigned int readRegister(byte reg) 
+unsigned int read_register(byte reg)
 {
 	byte toRead = (READ | reg);
 
@@ -89,16 +88,15 @@ unsigned int readRegister(byte reg)
 }
 
 /**
- * @brief 
+ * @brief La fonction écrit une valeur dans un registre.
  * 
- * @param reg 
- * @param value1 
- * @param value2 
+ * @param reg Registre dans lequel on veut envoyer les byte 1 et 2 combinés.
+ * @param value1 byte 1 (MSB)
+ * @param value2 byte 2 (LSB)
  */
-void writeRegister(byte reg, byte value1, byte value2) 
+void write_register(byte reg, byte value1, byte value2) 
 {
-
-	byte toSend = WRITE | reg;
+	byte toSend = (WRITE | reg);
 
 	SPI.beginTransaction(SPISettings(maxSpeed, MSBFIRST, SPI_MODE0));
 	digitalWrite(CS, HIGH);
