abbad7c5d5dee6c81719c127740731df447fa709
noemiefp <noemie.filion.p@gmail.com>
2023-01-30 12:27:07 -0500

    master essai 1

--- MotorControl/I2C_MasterCode/src/main.cpp
+++ MotorControl/I2C_MasterCode/src/main.cpp
@@ -1,9 +1,39 @@
 #include <Arduino.h>
+#include <Wire.h>
 
-void setup() {
-  // put your setup code here, to run once:
+/**
+byte angle = 30;
+#define ADRESSE 2
+
+void setup() 
+{
+  Wire.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+  Wire.beginTransmission(ADRESSE);
+  Wire.write(angle);
+  Wire.endTransmission();
+
+  angle++;
+  delay(100);
+}
+**/
+
+void setup()
+{
+  Wire.begin(); // join i2c bus (address optional for master)
+}
+
+byte x = 0;
+
+void loop()
+{
+  Wire.beginTransmission(4); // transmit to device #4
+  Wire.write(x);              // sends one byte  
+  Wire.endTransmission();    // stop transmitting
+
+  x++;
+  delay(500);
 }
