343132bcd5fe6cfca9b329be79695b52f33db173
tadamo3 <truizw@gmail.com>
2023-02-09 19:24:51 -0500

    Initial update with working I2C code

---
+++ MotorControl/I2C_MasterCode/include/main.h
@@ -0,0 +1,22 @@
+/**
+ * @file
+ * main.h
+ * 
+ * @brief
+ * Header file for main file of the master TeensyLC code
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <Arduino.h>
+#include <Wire.h>
+
+/* CONSTANTS */
+#define ADDRESS_MOTOR_1 0x01
+#define VITESSE 0x01
+
+#endif /* _MAIN_H_ */
--- MotorControl/I2C_MasterCode/src/main.cpp
+++ MotorControl/I2C_MasterCode/src/main.cpp
@@ -1,9 +1,37 @@
-#include <Arduino.h>
+/**
+ * @file
+ * main.cpp
+ * 
+ * @brief
+ * Main file for the I2C master code
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
 
-void setup() {
-  // put your setup code here, to run once:
+#include "main.h"
+
+/**
+ * @brief Setup function for main loop - Code executes at Teensy startup
+ */
+void setup(void) 
+{
+  /* Start as master on I2C bus */
+  Wire.begin();
+
+  Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+/**
+ * @brief Main loop of I2C master code application
+ */
+void loop(void) 
+{
+  Wire.beginTransmission(ADDRESS_MOTOR_1);
+  /* TODO: Écrire 3 bytes sur le fil (3 fois Wire.write) */
+  
+  /* TODO: Recevoir la confirmation que le moteur a reçu la vitesse */
+  Wire.endTransmission();
+  
+  delay(100);
 }
